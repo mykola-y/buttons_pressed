9db61fb5f7b4b8eb9281abe8814222d959612817
mykola <mykola.y@gmail.com>
2016-11-08 22:35:47 +0200

    123

---
+++ Debug/Src/gpio.d
@@ -0,0 +1,79 @@
+Src/gpio.o: ../Src/gpio.c \
+ /Users/mykola/Documents/TMaker/workspace/04/Inc/gpio.h \
+ /Users/mykola/Documents/TMaker/workspace/04/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h \
+ /Users/mykola/Documents/TMaker/workspace/04/Inc/stm32f4xx_hal_conf.h \
+ /Users/mykola/Documents/TMaker/workspace/04/Inc/main.h \
+ /Users/mykola/Documents/TMaker/workspace/04/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h \
+ /Users/mykola/Documents/TMaker/workspace/04/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h \
+ /Users/mykola/Documents/TMaker/workspace/04/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h \
+ /Users/mykola/Documents/TMaker/workspace/04/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h \
+ /Users/mykola/Documents/TMaker/workspace/04/Drivers/CMSIS/Include/core_cm4.h \
+ /Users/mykola/Documents/TMaker/workspace/04/Drivers/CMSIS/Include/core_cmInstr.h \
+ /Users/mykola/Documents/TMaker/workspace/04/Drivers/CMSIS/Include/cmsis_gcc.h \
+ /Users/mykola/Documents/TMaker/workspace/04/Drivers/CMSIS/Include/core_cmFunc.h \
+ /Users/mykola/Documents/TMaker/workspace/04/Drivers/CMSIS/Include/core_cmSimd.h \
+ /Users/mykola/Documents/TMaker/workspace/04/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h \
+ /Users/mykola/Documents/TMaker/workspace/04/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h \
+ /Users/mykola/Documents/TMaker/workspace/04/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h \
+ /Users/mykola/Documents/TMaker/workspace/04/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h \
+ /Users/mykola/Documents/TMaker/workspace/04/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h \
+ /Users/mykola/Documents/TMaker/workspace/04/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h \
+ /Users/mykola/Documents/TMaker/workspace/04/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h \
+ /Users/mykola/Documents/TMaker/workspace/04/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h \
+ /Users/mykola/Documents/TMaker/workspace/04/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h \
+ /Users/mykola/Documents/TMaker/workspace/04/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h \
+ /Users/mykola/Documents/TMaker/workspace/04/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h \
+ /Users/mykola/Documents/TMaker/workspace/04/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h \
+ /Users/mykola/Documents/TMaker/workspace/04/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h
+
+/Users/mykola/Documents/TMaker/workspace/04/Inc/gpio.h:
+
+/Users/mykola/Documents/TMaker/workspace/04/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal.h:
+
+/Users/mykola/Documents/TMaker/workspace/04/Inc/stm32f4xx_hal_conf.h:
+
+/Users/mykola/Documents/TMaker/workspace/04/Inc/main.h:
+
+/Users/mykola/Documents/TMaker/workspace/04/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc.h:
+
+/Users/mykola/Documents/TMaker/workspace/04/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_def.h:
+
+/Users/mykola/Documents/TMaker/workspace/04/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f4xx.h:
+
+/Users/mykola/Documents/TMaker/workspace/04/Drivers/CMSIS/Device/ST/STM32F4xx/Include/stm32f446xx.h:
+
+/Users/mykola/Documents/TMaker/workspace/04/Drivers/CMSIS/Include/core_cm4.h:
+
+/Users/mykola/Documents/TMaker/workspace/04/Drivers/CMSIS/Include/core_cmInstr.h:
+
+/Users/mykola/Documents/TMaker/workspace/04/Drivers/CMSIS/Include/cmsis_gcc.h:
+
+/Users/mykola/Documents/TMaker/workspace/04/Drivers/CMSIS/Include/core_cmFunc.h:
+
+/Users/mykola/Documents/TMaker/workspace/04/Drivers/CMSIS/Include/core_cmSimd.h:
+
+/Users/mykola/Documents/TMaker/workspace/04/Drivers/CMSIS/Device/ST/STM32F4xx/Include/system_stm32f4xx.h:
+
+/Users/mykola/Documents/TMaker/workspace/04/Drivers/STM32F4xx_HAL_Driver/Inc/Legacy/stm32_hal_legacy.h:
+
+/Users/mykola/Documents/TMaker/workspace/04/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_rcc_ex.h:
+
+/Users/mykola/Documents/TMaker/workspace/04/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio.h:
+
+/Users/mykola/Documents/TMaker/workspace/04/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_gpio_ex.h:
+
+/Users/mykola/Documents/TMaker/workspace/04/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma.h:
+
+/Users/mykola/Documents/TMaker/workspace/04/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_dma_ex.h:
+
+/Users/mykola/Documents/TMaker/workspace/04/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_cortex.h:
+
+/Users/mykola/Documents/TMaker/workspace/04/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash.h:
+
+/Users/mykola/Documents/TMaker/workspace/04/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ex.h:
+
+/Users/mykola/Documents/TMaker/workspace/04/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_flash_ramfunc.h:
+
+/Users/mykola/Documents/TMaker/workspace/04/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr.h:
+
+/Users/mykola/Documents/TMaker/workspace/04/Drivers/STM32F4xx_HAL_Driver/Inc/stm32f4xx_hal_pwr_ex.h:
